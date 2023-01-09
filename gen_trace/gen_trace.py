0560ba36add6acf7f8429c2daf26c5a6902a77bd
ffreemt <yucongo+fmt@gmail.com>
2023-01-09 11:50:40 +0800

    Update c_euclidean

--- gen_trace/gen_trace.py
+++ gen_trace/gen_trace.py
@@ -1,7 +1,31 @@
-"""Define gen_trace."""
-from logzero import logger
+"""Generate trace funtion given cset."""
+# pylint: disable=invalid-name
+from typing import Any, Callable, Tuple, Union
+from nptyping import NDArray, Shape, Float
+
+from scipy import interpolate
 
 
-def gen_trace():
-    """Define gen_trace."""
-    logger.debug(" entry ")
+def gen_trace(
+    cset: NDArray[Shape["Any, 3"], Float],
+    fill_value: Union[str, Tuple[int, int]] = "extrapolate",
+) -> Callable:
+    """Generate trace funtion given cset.
+
+    Args:
+        cset, clustered triple set, two-tuple set
+        fill_value: endpoint (along with (0, 0) for interp1d or using 'extrapolate'
+
+    Returns:
+        trace functions
+
+    """
+    x = cset.T[0]
+    y = cset.T[1]
+    if fill_value == "extrapolate":
+        trace = interpolate.interp1d(x, y, kind='linear', fill_value="extrapolate")
+    else:
+        [x_b, y_b], [x_e, y_e] = [0, 0], fill_value
+        trace = interpolate.interp1d([x_b] + x + [x_e], [y_b] + y + [y_e], kind='linear')
+
+    return trace
---
+++ gen_trace/plot_pset.py
@@ -0,0 +1,46 @@
+"""Plot pset/tset."""
+import matplotlib.pyplot as plt
+from  matplotlib.colors import LinearSegmentedColormap
+import numpy as np
+
+if "get_ipython" in globals():
+    plt.ion()
+
+
+def plot_pset(pset, scale=25, marker=None, cmap=None, alpha=1, new_figure=True):
+    """Plot pset/tset.
+
+    Args:
+        pset/test, nx3 np.array
+        scale, default 25, s=scale * pset.T[2]
+        cmap: default LinearSegmentedColormap.from_list('rg', ["red","darkgreen","green", "black"], N=10)
+        mark: default None,
+        alpha: default 1,
+        new_figure: default True, if True plt.figure()
+    """
+    # fig, ax = plt.subplots()
+    # ax.scatter...
+    if cmap is None:
+        cmap = LinearSegmentedColormap.from_list('rg', ["red","darkgreen","green"], N=10)
+
+    pset = np.array(pset)
+    if new_figure:
+        plt.figure()
+
+    plt.scatter(
+        pset.T[0],
+        pset.T[1],
+        s=scale * pset.T[2],
+        marker=marker,
+        vmin=0,
+        # cmap='viridis',
+        cmap=cmap,
+        # cmap='gray',
+        # c=df.z,
+        c=pset.T[2],
+        alpha=alpha,
+    )
+
+    if new_figure:
+        plt.grid()
+        plt.colorbar()
